6ab92eb4842b0e2948cd5238b8e6938a39395aa3
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-09-07 12:07:34 -0600

    created parent component invoicetable that will take in the child components of rows to be manipulated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { FaTrash, FaPlus} from "react-icons/fa";
 
+class InvoiceTable extends React.Component{
+    render(){
+        return
+        <div>Invoice Table</div>
+    }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
